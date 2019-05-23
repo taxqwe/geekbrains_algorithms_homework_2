4fe4457540cbe402c2559f32843d0dcde36ffee8
Nikolay Petrov <ni.petrov@qiwi.com>
2019-05-23 21:20:32 +0300

    added CustomTreeMap and test (homework)

---
+++ src/nikolay/homework6/CustomTreeMapTest.java
@@ -0,0 +1,67 @@
+package nikolay.homework6;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class CustomTreeMapTest {
+
+    @Test
+    public void example() {
+        CustomTreeMap<Character, Integer> myTreeMap = new CustomTreeMap<>();
+        myTreeMap.put('S', 1);
+        myTreeMap.put('E', 1);
+        myTreeMap.put('A', 1);
+        myTreeMap.put('R', 1);
+        myTreeMap.put('C', 1);
+        myTreeMap.put('H', 1);
+        myTreeMap.put('X', 1);
+        myTreeMap.remove('E');
+    }
+
+    @Test
+    public void heightTest() {
+        CustomTreeMap<Character, Integer> myTreeMap = new CustomTreeMap<>();
+        myTreeMap.put('S', 1);
+        myTreeMap.put('E', 1);
+        myTreeMap.put('A', 1);
+        myTreeMap.put('R', 1);
+        myTreeMap.put('C', 1);
+        myTreeMap.put('H', 1);
+
+        System.out.println(myTreeMap.height());
+
+        myTreeMap.removeMin();
+        myTreeMap.removeMin();
+        myTreeMap.removeMin();
+
+        System.out.println(myTreeMap.height());
+
+        System.out.println(myTreeMap.isBalanced());
+    }
+
+    @Test
+    public void homework() {
+        int countOfMaps = 20;
+
+        int balanced = 0;
+        for (int i = 0; i < countOfMaps; i++) {
+            CustomTreeMap<Integer, Integer> map = new CustomTreeMap<>();
+            fillMap(map);
+            if (map.isBalanced()) {
+                balanced++;
+            }
+        }
+
+        System.out.println("from " + countOfMaps + " CustomTreeMaps balanced is " + balanced + "\r\nnot balanced is " + (countOfMaps - balanced) + " trees");
+        System.out.println("not balanced is " + ((((double) balanced) * 100) / (double) countOfMaps) + " %");
+
+    }
+
+    private void fillMap(CustomTreeMap<Integer, Integer> customTreeMap) {
+        while (customTreeMap.height() < 6) {
+            customTreeMap.put(ThreadLocalRandom.current().nextInt(-100, 100), 0);
+        }
+    }
+}
